7189b9465e7ef76798503c56899e8484cbb328bd
yifen <anthonyeef@gmail.com>
2020-04-05 18:07:13 +0800

    feat: add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-59628523-3",
+      },
+    },
   ],
 }
