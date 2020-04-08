8a477fc76bfb5c49d8e1aebe867daef197e36b53
yifen <anthonyeef@gmail.com>
2020-04-08 21:57:03 +0800

    feat: add rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   siteMetadata: {
     title: "Yifen's notes",
+    siteUrl: "https://yifen.me/",
   },
   plugins: [
     `gatsby-plugin-sharp`,
@@ -52,6 +53,58 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-59628523-3",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+          siteMetadata {
+            title
+            siteUrl
+            site_url: siteUrl
+          }
+        }
+      }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                sort: {order: DESC, fields: [frontmatter___date]},
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields {slug}
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            output: "/rss.xml",
+            title: "RSS Feed",
+          },
+        ],
       },
     },
   ],
